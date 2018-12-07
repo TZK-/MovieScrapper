f7169101a19a0c2ebd8cf514a4bafb4816ad50f2
Thibault <thib-17@orange.fr>
2018-12-07 15:09:53 +0100

    Add infinite load

--- src/components/media-search/media-search.ts
+++ src/components/media-search/media-search.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {OmdbProvider} from "../../providers/omdb/omdb";
-import {NavController} from "ionic-angular";
+import {InfiniteScroll, NavController} from "ionic-angular";
 import {Media} from "../../Interfaces/Media";
 
 @Component({
@@ -12,6 +12,8 @@ export class MediaSearchComponent {
     @Input() detailPage : any;
 
     public medias: Array<Media>;
+    private search: string;
+    public page: number;
 
     constructor(private omdb: OmdbProvider) {
         this.reset();
@@ -19,15 +21,29 @@ export class MediaSearchComponent {
 
     onInput(event) {
         this.reset();
-
-        this.omdb.search(event.target.value, {type: this.searchType})
+        this.search = event.target.value;
+        
+        this.omdb.search(this.search, {type: this.searchType})
             .then((results: any) => this.medias = results)
             .catch((error) => {
                 this.reset();
-            })
+            });
     }
 
     private reset() {
         this.medias = [];
+        this.page = 1;
+    }
+
+    doInfinite(event: InfiniteScroll) {
+        this.omdb.search(this.search, {type: this.searchType, page: this.page + 1})
+            .then((results: any) => {
+                this.medias = [...this.medias, ...results];
+                this.page++;
+            })
+            .catch((error) => {
+                // do nothing
+            });
+        event.complete();
     }
 }
